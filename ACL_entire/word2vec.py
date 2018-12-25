d70d7e4d37b4ada2b7b358d9da3a6737e49b935c
XinyuLyu <xl422@scarletmail.rutgers.edu>
2018-12-25 14:46:38 +0800

    check test and train test data

--- ACL_entire/word2vec.py
+++ ACL_entire/word2vec.py
@@ -51,11 +51,12 @@ def get_dictionary(path):
 def embed_onehot(dictionary, data):
     result = []
     for word in data.split():
-        if word in dictionary:
-            index = dictionary[word]
-        else:
-            index = 0
-        result.append(index)#result.append(word)
+        #if word in dictionary:
+        #    index = dictionary[word]
+        #else:
+        #    index = 0
+        #result.append(index)
+        result.append(word)
     return result
 
 
